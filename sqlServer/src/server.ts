11f316d8a774ad6580bac2d32ea0bb9984172e3f
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-15 16:08:36 +0600

    SQL Data & neonDB create

--- sqlServer/src/server.ts
+++ sqlServer/src/server.ts
@@ -5,9 +5,26 @@ import express, {
 } from "express";
 const app: Application = express();
 const port = 3000;
+import { Pool } from "pg";
+
+app.use(express.json());
+// app.use(express.text())
+// app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_IUrePc9b3nXK@ep-
+  raspy-feather-ap55ccsk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=
+  require&channel_binding=require`,
+});
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express Server..");
+  res.status(200).json({ message: "Express Server.." });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  //   console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(201).json({ message: "Created", data: { name, email } });
 });
 
 app.listen(port, () => {
