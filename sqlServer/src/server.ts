f4442a2b7630f7904fadb781af0db4104071529c
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-15 23:21:06 +0600

    Set up Environment-based Configurations

--- sqlServer/src/server.ts
+++ sqlServer/src/server.ts
@@ -4,16 +4,17 @@ import express, {
   type Response,
 } from "express";
 const app: Application = express();
-const port = 3000;
+const port = config.port;
 import { Pool } from "pg";
 import sendRespond from "./utility/sendResponse";
+import config from "./config";
 
 app.use(express.json());
 // app.use(express.text())
 // app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_IUrePc9b3nXK@ep-raspy-feather-ap55ccsk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+  connectionString: config.connection_string,
 });
 
 const initDB = async () => {
