3fed45e31fa6a121a10f805ab308a554ca5fcaea
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-15 17:54:58 +0600

    Executing Pool and creating tables

--- sqlServer/src/server.ts
+++ sqlServer/src/server.ts
@@ -12,11 +12,28 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_IUrePc9b3nXK@ep-
-  raspy-feather-ap55ccsk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=
-  require&channel_binding=require`,
+  connectionString: `postgresql://neondb_owner:npg_IUrePc9b3nXK@ep-raspy-feather-ap55ccsk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Express Server.." });
 });
