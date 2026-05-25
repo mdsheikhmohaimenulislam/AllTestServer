00dc538d6ad940000c7d046f7c24fc4b3bc21cb0
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-25 20:31:43 +0600

    sendRespon added send successfylly done

--- sqlServer/src/utility/sendResponse.ts
+++ sqlServer/src/utility/sendResponse.ts
@@ -26,13 +26,13 @@ type TRespond<T> = {
   error?: unknown;
 };
 
-// const sendRespond = <T>(res: Response, data: TRespond<T>) => {
-//   return res.status(data.status).json({
-//     success: data.success,
-//     message: data.message,
-//     data: data.data,
-//     error: data.error,
-//   });
-// };
+const sendRespond = <T>(res: Response, data: TRespond<T>) => {
+  return res.status(data.status).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+    error: data.error,
+  });
+};
 
 export default sendRespond;
