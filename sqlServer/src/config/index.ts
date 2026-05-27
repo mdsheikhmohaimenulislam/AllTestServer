b4294d3723ca560ff21ec2a3e0589b7ec238f905
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-28 05:39:56 +0600

    dotenv.config path join  create done

--- sqlServer/src/config/index.ts
+++ sqlServer/src/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import path from "path";
 
-// dotenv.config({
-//   path: path.join(process.cwd(), ".env"),
-// });
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
 
 const config = {
   connection_string: process.env.CONNECTIONSTRING,
