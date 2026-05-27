f38ac403b8ac877db443814d753f6c796b312b31
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-27 16:28:34 +0600

    create jwt secret done

--- sqlServer/src/config/index.ts
+++ sqlServer/src/config/index.ts
@@ -9,9 +9,9 @@ const config = {
   connection_string: process.env.CONNECTIONSTRING,
   port: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
-  refresh_secret: process.env.JWT_REFRESH_SECRET,
-  expiresInAccessToken: process.env.EXPIRES_IN_ACCESS_TOKEN,
-  refreshInToken: process.env.EXPIRES_IN_REFRESH_TOKEN,
+  // refresh_secret: process.env.JWT_REFRESH_SECRET,
+  // expiresInAccessToken: process.env.EXPIRES_IN_ACCESS_TOKEN,
+  // refreshInToken: process.env.EXPIRES_IN_REFRESH_TOKEN,
 };
 
 export default config;
