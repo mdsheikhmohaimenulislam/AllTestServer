e2a67c91c75f038cdd9cc9dde8b32632edf5433f
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-28 05:42:54 +0600

    route autho added role

--- sqlServer/src/modules/user/user.route.ts
+++ sqlServer/src/modules/user/user.route.ts
@@ -11,6 +11,6 @@ route.post("/", userController.createUser);
 route.get("/", auth(role.admin, role.user), userController.getAllUsers);
 route.get("/:id", userController.getSingleUser);
 route.put("/:id", userController.updatedUser);
-// route.delete("/:id", userController.userDeleted);
+route.delete("/:id", userController.userDeleted);
 
 export const userRoute = route;
