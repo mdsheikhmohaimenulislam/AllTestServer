bff7005b4eb43196813594b484e149117254757a
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-29 20:39:40 +0600

    declare global request

--- sqlServer/src/middleware/index.d.ts
+++ sqlServer/src/middleware/index.d.ts
@@ -1,9 +1,9 @@
-// import type { JwtPayload } from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
 
-// declare global{
-//     namespace Express{
-//         interface Request{
-//             user?:JwtPayload
-//         }
-//     }
-// }
+declare global{
+    namespace Express{
+        interface Request{
+            user?:JwtPayload
+        }
+    }
+}
