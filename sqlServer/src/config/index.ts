233e8ca9ec2222313efc950a6b31e3db6c8ed462
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-04 08:15:07 +0600

    Config create done

--- sqlServer/src/config/index.ts
+++ sqlServer/src/config/index.ts
@@ -5,13 +5,13 @@ dotenv.config({
   path: path.join(process.cwd(), ".env"),
 });
 
-const config = {
-  connection_string: process.env.CONNECTIONSTRING,
-  port: process.env.PORT,
-  JWT_SECRET: process.env.JWT_SECRET,
-  refresh_secret: process.env.JWT_REFRESH_SECRET,
-  expiresInAccessToken: process.env.EXPIRES_IN_ACCESS_TOKEN,
-  refreshInToken: process.env.EXPIRES_IN_REFRESH_TOKEN,
-};
+// const config = {
+//   connection_string: process.env.CONNECTIONSTRING,
+//   port: process.env.PORT,
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   refresh_secret: process.env.JWT_REFRESH_SECRET,
+//   expiresInAccessToken: process.env.EXPIRES_IN_ACCESS_TOKEN,
+//   refreshInToken: process.env.EXPIRES_IN_REFRESH_TOKEN,
+// };
 
-export default config;
+// export default config;
