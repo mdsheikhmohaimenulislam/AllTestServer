12ab284d38577df9e08cb0330d9b3877a24fea98
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-05 22:14:57 +0600

    updatedUserIntoDB

--- sqlServer/src/modules/user/user.service.ts
+++ sqlServer/src/modules/user/user.service.ts
@@ -50,19 +50,19 @@ const getSingleUserIntoDB = async (id: string) => {
   return result;
 };
 
-const updatedUserIntoDB = async (payload: IUser, id: string) => {
-  const { name, password, age, is_active } = payload;
+// const updatedUserIntoDB = async (payload: IUser, id: string) => {
+//   const { name, password, age, is_active } = payload;
 
-  const result = await pool.query(
-    `
-    UPDATE users SET name=COALESCE($1,name),password=COALESCE($2,password),age=COALESCE($3,age),is_active=COALESCE($4,is_active)
-    WHERE id=$5 RETURNING *
-    `,
-    [name, password, age, is_active, id],
-  );
+//   const result = await pool.query(
+//     `
+//     UPDATE users SET name=COALESCE($1,name),password=COALESCE($2,password),age=COALESCE($3,age),is_active=COALESCE($4,is_active)
+//     WHERE id=$5 RETURNING *
+//     `,
+//     [name, password, age, is_active, id],
+//   );
 
-  return result;
-};
+//   return result;
+// };
 
 const userDeletedIntoDB = async (id: string) => {
   const result = await pool.query(
