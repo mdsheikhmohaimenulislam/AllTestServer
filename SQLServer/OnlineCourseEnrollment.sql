932c2d8e370afa5ef6a3cb2e7e42ed413f30b96c
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-13 23:33:40 +0600

    create courses and enrollments table

--- SQLServer/OnlineCourseEnrollment.sql
+++ SQLServer/OnlineCourseEnrollment.sql
@@ -8,3 +8,20 @@ create table students (
   country varchar(50),
   enrollment_date date
 )
+
+create table courses (
+  course_id int primary key,
+  course_title varchar(150),
+  category varchar(50),
+  price decimal(10, 2),
+  instructor varchar(100),
+  published_year int
+)
+create table enrollments (
+  enrollment_id int primary key,
+  student_id int references students (student_id),
+  course_id int references courses (course_id),
+  enrollment_date date,
+  progress_percentage int,
+  paid_amount decimal(10, 2)
+)
