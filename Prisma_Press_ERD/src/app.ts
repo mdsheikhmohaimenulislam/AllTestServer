fcc0669eb9ef8766063dc91f8d4cdfcde478b627
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-24 12:26:51 +0600

    creaate user & profile prisma

--- Prisma_Press_ERD/src/app.ts
+++ Prisma_Press_ERD/src/app.ts
@@ -1,8 +1,22 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
+import cors from "cors";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res:Response) => {
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
