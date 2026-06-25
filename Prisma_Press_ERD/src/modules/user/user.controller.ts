6ab27c76872d8c4a21d55a520deb7ddcf9931dc5
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-06-25 13:16:55 +0600

    21-7 Implementing Auth Middleware For Role Based Access Control In APIs

--- Prisma_Press_ERD/src/modules/user/user.controller.ts
+++ Prisma_Press_ERD/src/modules/user/user.controller.ts
@@ -3,7 +3,6 @@ import HttpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import jwt from "jsonwebtoken";
 import config from "../../config";
 import { jwtUtils } from "../../utils/jwt";
 import httpStatus from "http-status";
@@ -60,20 +59,21 @@ const registerUser = catchAsync(
 
 const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { accessToken } = req.cookies;
+    // const { accessToken } = req.cookies;
     // console.log(accessToken);
 
-    const verifiedToken = jwtUtils.verifyToken(
-      accessToken,
-      config.jwt_access_secret,
-    );
-    // console.log(verifiedToken);
 
-    if (typeof verifiedToken === "string") {
-      throw new Error(verifiedToken);
-    }
+    // const verifiedToken = jwtUtils.verifyToken(
+    //   accessToken,
+    //   config.jwt_access_secret,
+    // );
+    // // console.log(verifiedToken);
 
-    const profile = await userService.getMyProfileFromDB(verifiedToken.id);
+    // if (typeof verifiedToken === "string") {
+    //   throw new Error(verifiedToken);
+    // }
+
+    const profile = await userService.getMyProfileFromDB(req.user?.id as string);
 
     sendResponse(res, {
       success: true,
--- Prisma_Press_ERD/src/modules/user/user.route.ts
+++ Prisma_Press_ERD/src/modules/user/user.route.ts
@@ -1,12 +1,62 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
-
-
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import { Role } from "../../../generated/prisma/enums";
+import httpStatus from "http-status";
 
 const route = Router();
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        email: string;
+        id: string;
+        name: string;
+        role: Role;
+      };
+    }
+  }
+}
+
 route.post("/register", userController.registerUser);
-route.get("/me",userController.getMyProfile);
+route.get(
+  "/me",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    );
 
+    if (typeof verifiedToken === "string") {
+      throw new Error(verifiedToken);
+    }
 
-export const userRouters = route
+    const { name, email, id, role } = verifiedToken;
+
+    const requiredRoles = [Role.ADMIN, Role.AUTHOR, Role.USER];
+
+    if (!requiredRoles.includes(role)) {
+      return res.status(403).json({
+        success: true,
+        statusCode: httpStatus.FORBIDDEN,
+        message:
+          "Forbidden, yot don't have permission to access this resource.",
+      });
+    }
+
+    req.user = {
+      email,
+      id,
+      name,
+      role,
+    };
+
+    next();
+  },
+  userController.getMyProfile,
+);
+
+export const userRouters = route;
