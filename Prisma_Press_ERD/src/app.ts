7b895febddd26a70b61eeea461a49c5086f02ab6
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-07-02 16:43:50 +0600

    24-6 Setting Up Stripe Webhook Endpoint Route For Subscription

--- Prisma_Press_ERD/src/app.ts
+++ Prisma_Press_ERD/src/app.ts
@@ -9,6 +9,7 @@ import { commentRouter } from "./modules/comment/comment.route";
 import { notFound } from "./middleWares/notFound";
 import { globalErrorHandling } from "./middleWares/globalErrorHandler";
 import { subscriptionRouters } from "./modules/subscription/subscription.route";
+import { stripe } from "./lib/stripe";
 
 const app: Application = express();
 
@@ -17,6 +18,55 @@ app.use(
     origin: config.app_url,
     credentials: true,
   }),
+);
+
+const endpointSecret = config.stripe_webhook_secret;
+
+app.use(
+  "/api/subscription/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    let event = request.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers["stripe-signature"]!;
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret,
+        );
+      } catch (err: any) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`,
+        );
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  },
 );
 
 app.use(express.json());
@@ -32,9 +82,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
 app.use("/api/subscription", subscriptionRouters);
-
-
-
 
 app.use(notFound);
 
